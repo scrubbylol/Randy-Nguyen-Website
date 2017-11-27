b601439b02e32e08e873dde98a3e157f28f9aa91
Randy Nguyen <nguyenr@mail.uoguelph.ca>
2017-11-27 02:06:20 -0500

    php changes

--- php/connection.php
+++ php/connection.php
@@ -1,7 +1,7 @@
 <?php
-	$servername = "127.0.0.1"; //replace it with your database server name
-	$username = "root";  //replace it with your database username
-	$password = "";  //replace it with your database password
+	$servername = "192.168.0.10"; //replace it with your database server name
+	$username = "dotf";  //replace it with your database username
+	$password = "dotf";  //replace it with your database password
 	$dbname = "dotf_hs";
 	// Create connection
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
